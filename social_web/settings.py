7ce240de0a93b57507ef151482fa556379fda0f8
Magda Slifierz <m.slifierz@gmail.com>
2023-03-24 18:04:07 -0700

    chapter 5

--- social_web/settings.py
+++ social_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+^t^ktel4gba92z94(z==26w+6gudw-fgb&y-qcaugbi*@7^b4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +125,28 @@ STATIC_URL = 'static/'
 AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
+ 'social_core.backends.facebook.FacebookOAuth2',
+ 'social_core.backends.google.GoogleOAuth2',
+
 ]
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '539486128297602' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '4b2a4f9afd215e8bd0d16659450171e2'
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1054694248482-c7smqeqfva2nh67206khuamp1cntjmgv.apps.googleusercontent.com' # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
