698eb81c992131e1f35e514b54dee84e18dbcd34
Magda Slifierz <m.slifierz@gmail.com>
2023-03-28 18:50:21 -0700

    ex

--- images/views.py
+++ images/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImagesCreateForm
 from .models import Images
 
@@ -32,5 +35,46 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Images, id=id, slug=slug)
     return render(request, 'images/image/detail.html',
-                   {'section': 'images', 'image':image})
+                  {'section': 'images',
+                   'image': image})
 
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Images.objects.get(id=image_id)
+            if action == 'like':
+                image.user_like.add(request.user)
+            else:
+                image.user_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Images.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Images.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html',
+                      {'section':'images', 'images': images})
+    return render(request, 'images/image/list_images.html',
+                      {'section':'images', 'images': images})
